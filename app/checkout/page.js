8421e99ea94e3f3d4e6960e655e1cd4266a39374
horlayinkahtechie <horlainkah2005@gmail.com>
2025-05-03 20:06:33 +0100

    .

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,9 +1,14 @@
 import Checkout from "./checkout";
+import { Suspense } from "react";
+
+const PaymentPage = dynamic(() => import("./payment"), { ssr: false });
 
 export default function CheckoutPage() {
   return (
     <div>
-      <Checkout />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Checkout />
+      </Suspense>
     </div>
   );
 }
